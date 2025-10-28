1a0a6497f024f4da3fa7c28b44318753ccb7299f
Sean Bernardo <seanbenedict7326@gmail.com>
2025-10-28 12:12:33 +0800

    change: moved round-robin's time quantum logic to be attached to the CPU class

--- src/model/readyqueue/RoundRobin.cpp
+++ src/model/readyqueue/RoundRobin.cpp
@@ -2,18 +2,5 @@
 
 class RoundRobin : public ReadyQueue
 {
-    int timeQuantum;
-
 public:
-    RoundRobin(int tq) : timeQuantum(tq)
-    {
-    }
-
-    /**
-     * Gets the time quantum for the round-robin scheduler
-     */
-    int getTimeQuantum() const
-    {
-        return timeQuantum;
-    }
 };
