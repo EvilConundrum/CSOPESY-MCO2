14b5e54004fcca7b7505469baa17aa9db66f9ca3
Clive Ang <clive_jarel_c_ang@dlsu.edu.ph>
2025-10-28 17:31:29 +0800

    feat: add MO2 config to implementation

--- src/model/Config.cpp
+++ src/model/Config.cpp
@@ -6,6 +6,7 @@
 
 class Config
 {
+    // MO1 configurations
     int num_cpus;
     std::string scheduler_algorithm;
     int quantum_cycles;
@@ -13,9 +14,13 @@ class Config
     int min_instructions;
     int max_instructions;
     int delay_per_exec;
-
-    // basically time between per instruction in milliseconds
     int time_between_instructions;
+
+    // MO2 configurations
+    int max_overall_mem;
+    int mem_per_frame;
+    int min_mem_per_proc;
+    int max_mem_per_proc;
 
 public:
     Config(const std::string &filepath)
@@ -24,18 +29,26 @@ public:
         this->loadConfig(filepath);
 
         if (this->time_between_instructions == -1)
-            // set to 100ms default
-            this->time_between_instructions = 100;
+        {
+            this->time_between_instructions = 100; // set to 100ms default
+        }
     }
 
+    // MO1 getters
     int getNumCpus() const { return num_cpus; }
+    std::string getSchedulerAlgorithm() const { return scheduler_algorithm; }
     int getQuantumCycles() const { return quantum_cycles; }
     int getBatchProcessFreq() const { return batch_process_freq; }
     int getMinInstructions() const { return min_instructions; }
     int getMaxInstructions() const { return max_instructions; }
     int getDelayPerExec() const { return delay_per_exec; }
     int getTimeBetweenInstructions() const { return time_between_instructions; }
-    std::string getSchedulerAlgorithm() const { return scheduler_algorithm; }
+
+    // MO2 getters
+    int getMaxOverallMem() const { return max_overall_mem; }
+    int getMemPerFrame() const { return mem_per_frame; }
+    int getMinMemPerProc() const { return min_mem_per_proc; }
+    int getMaxMemPerProc() const { return max_mem_per_proc; }
 
     void loadConfig(const std::string &filepath)
     {
@@ -84,5 +97,13 @@ private:
             this->delay_per_exec = std::stoi(value);
         else if (key == "time-between-instructions")
             this->time_between_instructions = std::stoi(value);
+        else if (key == "max-overall-mem")
+            this->max_overall_mem = std::stoi(value);
+        else if (key == "mem-per-frame")
+            this->mem_per_frame = std::stoi(value);
+        else if (key == "min-mem-per-proc")
+            this->min_mem_per_proc = std::stoi(value);
+        else if (key == "max-mem-per-proc")
+            this->max_mem_per_proc = std::stoi(value);
     }
 };
