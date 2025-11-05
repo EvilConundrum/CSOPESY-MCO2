2f78d261b0d12ca66866cd41570cfa93ab3086a8
Clive Ang <clive_jarel_c_ang@dlsu.edu.ph>
2025-11-05 20:24:41 +0800

    chore: minor refactoring

--- src/model/readyqueue/FCFS.cpp
+++ src/model/readyqueue/FCFS.cpp
@@ -4,7 +4,6 @@
  * FCFS (First Come First Serve) Scheduler
  * - Non-preemptive scheduling algorithm
  * - Processes are executed in the order they arrive
- * - Simple queue-based implementation (inherits from ReadyQueue)
  */
 class FCFS : public ReadyQueue
 {
